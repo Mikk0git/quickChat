cf94b040ae6e25d8979aee1585df97c9e2421de1
Mikk0git <kasprzakmilan1@gmail.com>
2023-12-09 21:20:02 +0100

    simple get

--- server/server.py
+++ server/server.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
+@app.route("/get-messages/<room_id>")
+def get_messages(room_id):
+    messages = {
+        "message1" : {
+            "text" : "dupa123",
+            "senderId" : "20133"
+        }
+    }
+
+    return jsonify(messages), 200
 
 
 
