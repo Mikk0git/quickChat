24a1eef47cf0da76956f964b09496494626ca696
Mikk0git <kasprzakmilan1@gmail.com>
2023-12-10 21:02:24 +0100

    cors

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 DB_NAME = "qcDatabase.db"
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 db.init_app(app)
